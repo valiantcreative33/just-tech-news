c4a93fb8df68cef464a54cb047284e7b2f388d62
Ruben Matamoros <ruben@valiantcreative.net>
2021-02-04 12:16:44 -0500

    created the api routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const commentRoutes = require('./comment-routes');
 
 router.use('/api', apiRoutes);
+router.use('/comments', commentRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
