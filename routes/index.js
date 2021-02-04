7f4977d2ae192f2c4e42c94febd9ee94946e2510
Ruben Matamoros <ruben@valiantcreative.net>
2021-02-04 13:45:52 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const commentRoutes = require('./comment-routes');
 
 router.use('/api', apiRoutes);
-router.use('/comments', commentRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
